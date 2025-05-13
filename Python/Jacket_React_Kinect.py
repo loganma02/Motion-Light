0228e062684f35102005db4c2618a038637dc63b
loganma02 <147100908+loganma02@users.noreply.github.com>
2025-05-13 01:19:34 -0700

    Got color to depth working with perspective transform, also changed to multiple windows rather than vstack. Will keep some code commented out in case

--- Python/Jacket_React_Kinect.py
+++ Python/Jacket_React_Kinect.py
@@ -25,22 +25,25 @@ from pykinect2024 import PyKinect2024
 FRAME_WIDTH = 1920 # Kinect v2 color feed resolution
 FRAME_HEIGHT = 1080
 
-DEVICES = pd.DataFrame(columns=['name', 'ip', 'numLED', 'markerID'])
+InfraredPoints = np.float32([[221, 138], [218, 300], [86, 301], [91, 133]])
+ColorPoints = np.float32([[784, 339], [761, 814], [371, 806], [396, 313]])
+perspectiveTransform = cv2.getPerspectiveTransform(ColorPoints, InfraredPoints)
+
 ROLLING_AVERAGE_FRAMES = 5
 ENCODING = 'utf-8' #Encoding the qr codes are in
 WLED_PORT = 21324
 VIDEO_MODE = 'color' # Currently support the color and infrared feeds from kinect
+DISPLAY_DEPTH = True
 FILENAME = 'DEVICES.pkl'
 LOAD = True
 
 if LOAD:
     DEVICES = pd.read_pickle(FILENAME)
+else:
+    DEVICES = pd.DataFrame(columns=['name', 'ip', 'numLED', 'markerID'])
 
 import cv2.aruco as aruco
-# Choose your dictionary — 4x4 with 50 unique markers is good for quick testing
 aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
-
-# Create the detector using the newer Detector class
 parameters = aruco.DetectorParameters()
 detector = aruco.ArucoDetector(aruco_dict, parameters)
 
@@ -53,12 +56,8 @@ elif (VIDEO_MODE == 'infrared'):
     FRAME_HEIGHT = 424
 else:
     raise ValueError('Check video mode, should be color or infrared.')
+kinectInfra = PyKinectRuntime.PyKinectRuntime(PyKinect2024.FrameSourceTypes_Infrared)
 kinectDepth = PyKinectRuntime.PyKinectRuntime(PyKinect2024.FrameSourceTypes_Depth)
-
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
-cap.set(cv2.CAP_PROP_FPS, 30)
 
 while True:
     if (VIDEO_MODE == 'infrared'):
@@ -67,17 +66,22 @@ while True:
         frame = (frame / 256).astype(np.uint8)
     elif (VIDEO_MODE == 'color'):
         frame = kinect.get_last_color_frame()
+        #frameInfra = kinectInfra.get_last_infrared_frame()
         frame = frame.reshape((1080, 1920, 4))
-        frame = frame[0:FRAME_HEIGHT, 308:FRAME_WIDTH - 308]
     frame = cv2.flip(frame, 1)
+
     frameDepth = kinectDepth.get_last_depth_frame()
     frameDepth = frameDepth.reshape((424, 512))
     frameDepth = cv2.flip(frameDepth, 1)
-    #frameDepth = cv2.resize(frameDepth, (FRAME_HEIGHT, FRAME_WIDTH))
+
+    # Saving this for taking more pictures to get perspective transform
+    # frameInfra = kinectInfra.get_last_infrared_frame()
+    # frameInfra = frameInfra.reshape((424, 512))
+    # frameInfra = cv2.flip(frameInfra, 1)
+    # frameDepth = cv2.resize(frameDepth, (FRAME_HEIGHT, FRAME_WIDTH))
 
     corners, ids, rejected = detector.detectMarkers(frame)
-    #print(ids)
-    depthX = 0
+    depthX = 0 #TODO: make this support multiple codes, may just need to process depth frame after all
     depthY = 0
     if ids is not None:
         for i, marker_id in enumerate(ids.flatten()):
@@ -85,9 +89,11 @@ while True:
             corner_pts = corners[i][0]
             centerX = int(np.mean(corner_pts[:, 0]))
             centerY = int(np.mean(corner_pts[:, 1]))
-            #depthX = int((centerX / FRAME_WIDTH) * 512)
-            depthX = int((centerX / 1304) * 512)
-            depthY = int((centerY / FRAME_HEIGHT) * 424)
+
+            point = np.array([[[centerX, centerY]]], dtype=np.float32)
+            transformedPoint = cv2.perspectiveTransform(point, perspectiveTransform)[0][0]
+            depthX = min(int(transformedPoint[0]), 511)
+            depthY = min(int(transformedPoint[1]), 423)
 
             depthValue = frameDepth[depthY, depthX]
             if depthValue > 500 and depthValue < 1500:
@@ -100,17 +106,13 @@ while True:
                 thisNumLed = thisRow.iloc[0]['numLED']
                 thisIP = thisRow.iloc[0]['ip']
 
-
                 rValue, gValue, bValue = position_to_rgb(centerX, FRAME_WIDTH)
                 cv2.circle(frame, (centerX, centerY), 10, (bValue, gValue, rValue), cv2.FILLED) # If found in devices put circle on center with transmit color
                 cv2.putText(frame, f"{decodedData}, int:{intensity}, depth: {depthValue}", ((centerX+20), centerY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (bValue, gValue, rValue), 2) # Name on code with matching color
                 cv2.putText(frame, f"R: {rValue}, G:{gValue}, B:{bValue}", ((centerX+20), (centerY+20)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (bValue, gValue, rValue), 2) # Name on code with matching color
 
                 led_data = bytearray(thisNumLed * 3)  # Initialize all LEDs to off
-
-                #print(f"Depth: {depthValue}, intensity: {intensity}")
                 for i in range(thisNumLed):
-                    # Set the LED's RGB values
                     led_data[i * 3] = int(rValue * intensity)
                     led_data[i * 3 + 1] = int(gValue * intensity)
                     led_data[i * 3 + 2] = int(bValue * intensity)
@@ -118,7 +120,7 @@ while True:
             else:
                 cv2.circle(frame, (centerX, centerY), 10, (0, 0, 0), cv2.FILLED)
                 cv2.putText(frame, f"{decodedData}, int:{intensity}, depth: {depthValue}", ((centerX + 20), centerY), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                            (0, 0, 0), 2)  # Name on code with matching color
+                            (0, 0, 0), 2)
 
 
     # scannedCodes = decode(frame)
@@ -148,35 +150,26 @@ while True:
     #             cv2.putText(frame, decodedData, ((centerX + 20), centerY), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
     #                         (0, 0, 0), 2)  # Name on code with matching color
 
-    # Display the output
-    frameDepth = cv2.normalize(frameDepth, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-    frameDepth = cv2.cvtColor(frameDepth, cv2.COLOR_GRAY2RGBA)
-    frameDepth = cv2.circle(frameDepth, (depthX, depthY), 10, (255, 255, 255), cv2.FILLED)
     if (VIDEO_MODE == 'color'):
         #frameDepthCopy = cv2.copyMakeBorder(frameDepth, 0, 0, 224, 224, cv2.BORDER_CONSTANT)
-        frame = cv2.resize(frame, (512, 424))
-        print(np.shape(frame))
-        print(np.shape(frameDepth))
-        comboFrame = np.vstack((frame, frameDepth))
+        frame = cv2.resize(frame, (0, 0), fx = 0.5, fy = 0.5)
     elif (VIDEO_MODE == 'infrared'):
-        comboFrame = np.vstack((frame, frameDepth))
+        frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGBA)
 
+    if DISPLAY_DEPTH: # Save the frameDepth processing if I dont want to display it
+        frameDepth = cv2.normalize(frameDepth, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+        frameDepth = cv2.cvtColor(frameDepth, cv2.COLOR_GRAY2RGBA)
+        frameDepth = cv2.circle(frameDepth, (depthX, depthY), 10, (255, 255, 255), cv2.FILLED)
+        cv2.imshow('Motion Light Depth', frameDepth)
 
-    #mapped = kinect.depth_frame_to_color_space(frameDepth)
-    #comboFrame = np.vstack((frame, frameDepthCopy))
-    cv2.imshow('Motion Light', comboFrame)
+    cv2.imshow('Motion Light Main', frame)
 
 
     # Break loop on 'q' key press
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        # print(f"These were the LEDCords: {LEDCords}")
-        # print(f"These were the awayCords: {awayCords}")
         print(f"Saving device list as {FILENAME}")
         DEVICES.to_pickle(FILENAME)
         print("Quitting...")
-        #print(frameDepth)
-        #print(normalized_depth)
-        #print(np.shape(frameDepth))
         break
 
     # Listen for refresh when r is pressed
@@ -187,9 +180,15 @@ while True:
         print('Done Refreshing!')
         print()
 
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        print('Saving images')
+        cv2.imwrite('infrared1.png', frameInfra)
+        cv2.imwrite('color1.png', frame)
+        print()
+
     if cv2.waitKey(1) & 0xFF == ord('p'):
         print(DEVICES.head())
 
-cap.release()
+# cap.release()
 cv2.destroyAllWindows()
 
